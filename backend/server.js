90e9bb3759b5bf977fc7487d47f5dd53d95f618d
differental <enderchen004@outlook.com>
2024-07-20 20:25:21 +0100

    Fix things

---
+++ backend/server.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+const express = require('express');
+//const axios = require('axios');
+const bodyParser = require('body-parser');
+const { ChatOpenAI } = require('@langchain/openai');
+
+const model = new ChatOpenAI({
+    model: "gpt-3.5-turbo",
+    temperature: 0.0,
+    apiKey: process.env.OPENAI_API_KEY
+});
+
+const app = express();
+const port = 3001;
+
+app.use(bodyParser.json());
+
+app.post('/api/gpt', async (req, res) => {
+    const userMessage = req.body.message;
+
+    try {
+        /*const response = await axios.post(
+        'https://api.openai.com/v1/engines/davinci-codex/completions',
+        {
+            prompt: userMessage,
+            max_tokens: 150,
+        },
+        {
+            headers: {
+            'Authorization': `Bearer YOUR_OPENAI_API_KEY`
+            }
+        }
+        );*/
+
+        //const gptMessage = response.data.choices[0].text.trim();
+        const gptMessage = await model.invoke(userMessage);
+        console.log(`Message received:`);
+        res.json({ reply: gptMessage });
+    } catch (error) {
+        res.status(500).send('Error communicating with GPT API');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
