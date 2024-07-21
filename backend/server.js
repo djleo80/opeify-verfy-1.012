0b029adaa8ae5dc16b9d872e62aea424feacf5f2
differental <enderchen004@outlook.com>
2024-07-21 07:57:26 +0100

    code update

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,118 @@ const port = 3001;
 
 app.use(bodyParser.json());
 
+const templateMainPrompt = `
+Given the user query delimited by <tag>, match it to the cloest category from the list of six categories below, then follow the given instructions for that specific category. 
+Only output in the format specified for each category. Always start your response with [. Do not put in \`\`\`.
+
+- Category: Request Transaction
+- Description: User query is asking for a transaction to be made to another account
+- Output: Json snippet in the following schema delimited by \`\`\`. 
+ 
+\`\`\`
+[
+    {
+        "type": "transaction",
+        "amount": number, // Amount of Polkadot to transfer. If not provided, put 0. 
+        "dest": string // A string of the destination cryptocurrency wallet address. If not provided. put '0'.
+    }
+]
+\`\`\`
+ 
+- Category: Account Details
+- Description: User query is asking for specific details related to their account
+- Output: Json snippet in the following schema delimited by \`\`\`. 
+ 
+\`\`\`
+[
+    {
+        "type": "account",
+        "query": string // User query
+    }
+]
+\`\`\`
+ 
+- Category: Blockchain Knowledge
+- Description: User query is asking for knowledge related to Web3, Blockchain or Cryptocurrency
+- Output: Json snippet in the following schema delimited by \`\`\`.
+ 
+\`\`\`
+[
+    {
+        "type": "blockchains",
+        "query": string // User query
+    }
+]
+\`\`\`
+ 
+- Category: Smart Contract Generation
+- Description: User query is generating smart contract
+- Output: Json snippet in the following schema delimited by \`\`\`.
+ 
+\`\`\`
+[
+    {
+        "type": "smart_contract",
+        "query": string // User query
+    }
+]
+\`\`\`
+ 
+- Category: General
+- Description: Any general query that does not fall into one of the categories above
+- Output:  Json snippet in the following schema delimited by \`\`\`.
+ 
+\`\`\`
+[
+    {
+        "type": "general",
+        "info": string // Answer the query in less than 50 words
+    }
+]
+\`\`\`
+
+<tag>{user_input}</tag>
+`;
+
+const blockchainPrompt = `
+You are a very smart Blockchains expert, and especially an expert in the Polkadot ecosystem. You are so good because you are able to explain difficulty concepts with relatively straightforward examples. \
+When answering questions, you should also make references to relevant knowledge inside Polkadot ecosystem, and try to introduce the user to the amazing world of Web3 for Economics with Polkadot. 
+
+Here is a question:
+{query}
+`
+
+const smartContractPrompt = `
+You are a very good smart contract maker, and especially an expert in the Polkadot ecosystem. \
+With the following request delimited by <tag>, generate a smart contract and output it as your response. 
+<tag>{query}</tag>
+`
+
+
+async function handlePrompt(user_input) {
+    const promptText = templateMainPrompt.replace('{user_input}', user_input);
+ 
+    const response = await model.invoke(promptText);
+
+    console.log(response.content)
+ 
+    try {
+        if (String(response.content).startsWith('```') && String(response.content).endsWith('```')) {
+            return JSON.parse(String(response.content).slice(3, -3))[0];
+        }
+        if (JSON.parse(String(response.content))[0]) {
+            return JSON.parse(String(response.content))[0];
+        }
+        return JSON.parse(String(response.content));
+    } catch (error) {
+        console.error(`[CRITICAL] Error handling initial prompt: `, error);
+        console.log(response.content);
+        return {};
+    }
+}
+
+
+
 async function sendTransaction(sender, receiver, amount) {
     const tx = api.tx.balances.transfer(receiver, amount);
     const result = await tx.signAndSend(sender);
@@ -35,11 +147,45 @@ app.post('/api/gpt', async (req, res) => {
     const userMessage = req.body.message;
 
     try {
-        blockchainMain().catch(console.error);
+        //blockchainMain().catch(console.error);
 
-        const gptMessage = await model.invoke(userMessage);
-        console.log(`Human: ${userMessage}\nGPT: ${gptMessage.content}`);
-        res.json({ reply: gptMessage.content });
+        const gptMessage = await handlePrompt(userMessage);
+        console.log(gptMessage);
+
+        if (gptMessage.type == 'general') {
+            console.log(`Human: ${userMessage}\nGPT (general): ${gptMessage.info}`);
+            res.json({ reply: gptMessage.info });
+        }
+        else if (gptMessage.type == 'transaction') {
+            // Transaction
+            console.log("Transaction");
+            const reply = `Sent ${gptMessage.amount} to ${gptMessage.dest}`;
+            console.log(`Human: ${userMessage}\nGPT (transaction): ${reply}`);
+            res.json({ reply: reply });
+        }
+        else if (gptMessage.type == 'account') {
+            // Account details
+            // get account info
+            res.json({ reply });
+        }
+        else if (gptMessage.type == 'blockchains') {
+            // Blockchain prompt
+            const promptText = blockchainPrompt.replace('{query}', gptMessage.query);
+            const response = await model.invoke(promptText);
+            res.json({ reply: response.content });
+        }
+        else if (gptMessage.type == 'smart_contract') {
+            // Smart contract prompt
+            const promptText = smartContractPrompt.replace('{query}', gptMessage.query);
+            const response = await model.invoke(promptText);
+            res.json({ reply: response.content });
+        }
+        else { //wrong
+            res.json({ reply: `Unable to process machine response.\n${gptMessage.content}` });
+        }
+
+        //console.log(`Human: ${userMessage}\nGPT: ${gptMessage.content}`);
+        //res.json({ reply: gptMessage.content });
     } catch (error) {
         res.status(500).send('Error communicating with GPT API');
     }
